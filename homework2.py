13e8c62135ce5022bd9b22df546b9cbd7219c9f8
bitoiu <vmrmonteiro@gmail.com>
2014-01-21 20:27:08 +0000

    It wasn't so hard wasn't it

--- homework2.py
+++ homework2.py
@@ -19,4 +19,18 @@
 # 4ca00ff4c898d61e1edbf1800618fb2828a226d160dad07883d04e008a7897ee \
 #     2e4b7465d5290d0c0e6c6822236e1daafb94ffe0c5da05d9476be028ad7c1d81
 
+from Crypto.Cipher import AES
+
+
+def decryptCBC(key, cipherText):
+    iv = cipherText[0:32].decode('hex')
+
+    crypto = AES.new(key.decode("hex"), AES.MODE_CBC, iv)
+    print crypto.decrypt(cipherText[32:].decode("hex"))
+
+
 if __name__ == '__main__':
+    key = "140b41b22a29beb4061bda66b6747e14"
+    cipherText = "4ca00ff4c898d61e1edbf1800618fb2828a226d160dad07883d04e008a7897ee2e4b7465d5290d0c0e6c6822236e1daafb94ffe0c5da05d9476be028ad7c1d81"
+
+    decryptCBC(key, cipherText)
