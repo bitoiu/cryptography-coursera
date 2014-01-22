979c063f49883fdc60cc740ddc21c620b556b68d
Vitor Monteiro <vmrmonteiro@gmail.com>
2014-01-22 17:11:14 +0000

    CBC implemented

--- homework2.py
+++ homework2.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python
 
+
 # In this project you will implement two encryption/decryption systems, one using AES in CBC mode and another using AES
 # in counter mode (CTR). In both cases the 16-byte encryption IV is chosen at random and is prepended to the ciphertext.
 # For CBC encryption we use the PKCS5 padding scheme discussed in class (13:50).
@@ -19,18 +20,86 @@
 # 4ca00ff4c898d61e1edbf1800618fb2828a226d160dad07883d04e008a7897ee \
 #     2e4b7465d5290d0c0e6c6822236e1daafb94ffe0c5da05d9476be028ad7c1d81
 
+# Question 1
+# CBC key: 140b41b22a29beb4061bda66b6747e14
+# CBC Ciphertext 1:
+# 4ca00ff4c898d61e1edbf1800618fb2828a226d160dad07883d04e008a7897ee \
+#     2e4b7465d5290d0c0e6c6822236e1daafb94ffe0c5da05d9476be028ad7c1d81
+# Answer for Question 1
+#
+# Question 2
+# CBC key: 140b41b22a29beb4061bda66b6747e14
+# CBC Ciphertext 2:
+# 5b68629feb8606f9a6667670b75b38a5b4832d0f26e1ab7da33249de7d4afc48 \
+#     e713ac646ace36e872ad5fb8a512428a6e21364b0c374df45503473c5242a253
+# Answer for Question 2
+#
+# Question 3
+# CTR key: 36f18357be4dbd77f050515c73fcf9f2
+# CTR Ciphertext 1:
+# 69dda8455c7dd4254bf353b773304eec0ec7702330098ce7f7520d1cbbb20fc3 \
+#     88d1b0adb5054dbd7370849dbf0b88d393f252e764f1f5f7ad97ef79d59ce29f5f51eeca32eabedd9afa9329
+# Answer for Question 3
+#
+# Question 4
+# CTR key: 36f18357be4dbd77f050515c73fcf9f2
+# CTR Ciphertext 2:
+# 770b80259ec33beb2561358a9f2dc617e46218c0a53cbeca695ae45faa8952aa \
+#     0e311bde9d4e01726d3184c34451
+# Answer for Question 4
+
+
 from Crypto.Cipher import AES
 
+HEXENC_BLOCKSIZE = 32
+
+problemSetCBC = [
+    {"key": "140b41b22a29beb4061bda66b6747e14",
+     "ct": "4ca00ff4c898d61e1edbf1800618fb2828a226d160dad07883d04e008a7897ee2e4b7465d5290d0c0e6c6822236e1daafb94ffe0c5da05d9476be028ad7c1d81"}
+  , {"key": "140b41b22a29beb4061bda66b6747e14",
+     "ct": "5b68629feb8606f9a6667670b75b38a5b4832d0f26e1ab7da33249de7d4afc48e713ac646ace36e872ad5fb8a512428a6e21364b0c374df45503473c5242a253"}]
+
+def autoDecryptCBC(key, cipherText):
+    iv = cipherText[0:32].decode('hex')
+    crypto = AES.new(key.decode("hex"), AES.MODE_CBC, iv)
+    print crypto.decrypt(cipherText[HEXENC_BLOCKSIZE:].decode("hex"))
+
+
+def aesECB(key, cipherBlock):
+    crypto = AES.new(key.decode("hex"), AES.MODE_ECB)
+    return crypto.decrypt(cipherBlock.decode("hex")).encode("hex")
+
+def hexxor(a, b):
+    from Crypto.Util.strxor import strxor
+    return strxor(a.decode('hex'), b.decode('hex')).encode('hex')
 
 def decryptCBC(key, cipherText):
-    iv = cipherText[0:32].decode('hex')
+    result = ""
+    iv = cipherText[0:HEXENC_BLOCKSIZE]
+    cipherText = cipherText[HEXENC_BLOCKSIZE:]
+    x = 0
 
-    crypto = AES.new(key.decode("hex"), AES.MODE_CBC, iv)
-    print crypto.decrypt(cipherText[32:].decode("hex"))
+    if len(cipherText) % 2 != 0:
+        print "WARN: Padding is needed"
+
+    while x < len(cipherText):
+        cipherBlock = cipherText[x:x + HEXENC_BLOCKSIZE]
+        blockCBCresult = aesECB(key, cipherBlock)
+
+        if x == 0:
+            result += hexxor(blockCBCresult, iv)
+        else:
+            result += hexxor(blockCBCresult, cipherText[x - HEXENC_BLOCKSIZE:x])
+
+        x += HEXENC_BLOCKSIZE
+
+    return result.decode('hex')
 
 
 if __name__ == '__main__':
-    key = "140b41b22a29beb4061bda66b6747e14"
-    cipherText = "4ca00ff4c898d61e1edbf1800618fb2828a226d160dad07883d04e008a7897ee2e4b7465d5290d0c0e6c6822236e1daafb94ffe0c5da05d9476be028ad7c1d81"
 
-    decryptCBC(key, cipherText)
+    for problem in problemSetCBC:
+        print decryptCBC(problem["key"], problem["ct"])
+
+    for problem in problemSetCTR:
+        print decryptCTR(problem["key"], problem["ct"])
