b206fd9e4f0306598dd1bf1fcf0ea71a06f263d5
bitoiu <vmrmonteiro@gmail.com>
2014-02-07 17:36:06 +0000

    All ok except padding for last block

--- homework4.py
+++ homework4.py
@@ -3,43 +3,70 @@ import homework2 as util
 
 TARGET_CIPHERTEXT = "f20bdba6ff29eed7b046d1df9fb7000058b1ffb4210a580f748b4ac714c001bd4a61044426fb515dad3f21f18aa577c0bdf302936266926ff37dbf7035d5eeb4"
 BLOCK_SIZE_IN_BYTES = 16
+BLOCK_SIZE_HEXENC = BLOCK_SIZE_IN_BYTES * 2
+
+
+def getCipherAdjustment(targetPad, intermediateBlock):
+
+    result = ""
+    for x in range(0, len(intermediateBlock),2):
+        result += util.hexxor(targetPad, intermediateBlock[x:x+2])
+    return result
 
 
 def padAttack(cipherText):
-    endPosition = len(cipherText)
-    startPosition = BLOCK_SIZE_IN_BYTES * 2
-    currentPT = ""
-    oracleQuery = pq.PaddingOracle()
+    ctLength = len(cipherText)
+    ctBlockLength = ctLength / BLOCK_SIZE_HEXENC
+    plaintText = ""
 
-    for index in range(endPosition, startPosition, -2):
-        step = (endPosition - index) / 2;
-        pad = chr(step).encode('hex') * (step + 1)
-        cipherSection = cipherText[index - 2:endPosition]
+    for blockIndex in range(0, ctBlockLength - 1):
 
-        if len(pad) != len(cipherSection):
-            raise Exception("CT suffix and pad should be same length")
+        if blockIndex < 2: continue
 
-        padXorCipher = util.hexxor(pad, cipherSection)
+        startPosition = blockIndex * BLOCK_SIZE_HEXENC
+        endPosition = startPosition + BLOCK_SIZE_HEXENC
+        oracleQuery = pq.PaddingOracle()
+        intermediateBlock = ""
+        blockPlainText = ""
 
-        for trialByte in [chr(x).encode('hex') for x in range(0, 256)]:
+        for index in range(endPosition, startPosition, -2):
+            step = (endPosition - index) / 2 + 1
+            stepHex = chr(step).encode('hex')
+            pad = stepHex * step
+            originalCipherByte = cipherText[index - 2:index]
 
-            trialSection = trialByte + currentPT
+            cipherAdjustment = getCipherAdjustment(stepHex, intermediateBlock)
 
-            if len(padXorCipher) != len(trialSection):
-                raise Exception("different lengths for trial byte and pad/cipher xor")
+            for trialByte in [chr(x).encode('hex') for x in range(0, 256)]:
 
-            trialPad = util.hexxor(trialSection,padXorCipher)
 
-            queryString = cipherText[0:index - 2] + trialPad
-            queryResult = oracleQuery.query(queryString)
+                firstPart = cipherText[0:index - 2]
+                lastPart = cipherText[endPosition:endPosition + 32]
+                queryString = firstPart + trialByte + cipherAdjustment + lastPart
+                print "{0} - {1} - {2} - {3}".format(firstPart, trialByte, cipherAdjustment, lastPart)
 
-            if queryResult == True:
-                print "Match with byte: ", trialByte
-                currentPT += trialByte
-                break
+                if len(queryString) % 32 != 0:
+                    raise Exception("Submitting a query which is not a multiple of 32 HexEncoded")
 
-    return currentPT
+                queryResult = oracleQuery.query(queryString)
 
+                if queryResult:
+                    intermediateByte = util.hexxor(trialByte, stepHex)
+                    intermediateBlock = intermediateByte + intermediateBlock;
+                    plainTextByte = util.hexxor(originalCipherByte, intermediateByte)
+                    blockPlainText = plainTextByte + blockPlainText
+
+                    print "Match with guess={0}".format(trialByte)
+                    print "Intermediate byte={0}".format(intermediateByte)
+                    print "Plaint text byte={0}".format(plainTextByte)
+                    break
+
+            if len(intermediateBlock) != len(pad):
+                raise Exception("No success finding a match for query")
+
+        plaintText = blockPlainText + plaintText
+
+    return plaintText
 
 
 if __name__ == '__main__':
--- paddingQuery.py
+++ paddingQuery.py
@@ -2,6 +2,7 @@
 
 import urllib2
 import sys
+import random
 
 TARGET = 'http://crypto-class.appspot.com/po?er='
 #--------------------------------------------------------------
@@ -9,15 +10,18 @@ TARGET = 'http://crypto-class.appspot.com/po?er='
 #--------------------------------------------------------------
 class PaddingOracle(object):
     def query(self, q):
+
+        #return random.randint(1, 2) == 1
         target = TARGET + urllib2.quote(q)    # Create query URL
         req = urllib2.Request(target)         # Send HTTP request to server
         try:
             f = urllib2.urlopen(req)          # Wait for response
         except urllib2.HTTPError, e:
-            print "We got: %d" % e.code       # Print response code
+            #print "We got: %d" % e.code       # Print response code
             if e.code == 404:
                 return True # good padding
             return False # bad padding
+
 
 if __name__ == "__main__":
     po = PaddingOracle()
