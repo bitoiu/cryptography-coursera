ca6152bf39617ff31b0bd4b974ac432bc30b473c
bitoiu <vmrmonteiro@gmail.com>
2014-02-08 14:37:56 +0000

    Omg, this last block was really painful , done now.

--- homework4.py
+++ homework4.py
@@ -7,10 +7,9 @@ BLOCK_SIZE_HEXENC = BLOCK_SIZE_IN_BYTES * 2
 
 
 def getCipherAdjustment(targetPad, intermediateBlock):
-
     result = ""
-    for x in range(0, len(intermediateBlock),2):
-        result += util.hexxor(targetPad, intermediateBlock[x:x+2])
+    for x in range(0, len(intermediateBlock), 2):
+        result += util.hexxor(targetPad, intermediateBlock[x:x + 2])
     return result
 
 
@@ -21,29 +20,28 @@ def padAttack(cipherText):
 
     for blockIndex in range(0, ctBlockLength - 1):
 
-        if blockIndex < 2: continue
-
         startPosition = blockIndex * BLOCK_SIZE_HEXENC
         endPosition = startPosition + BLOCK_SIZE_HEXENC
         oracleQuery = pq.PaddingOracle()
         intermediateBlock = ""
         blockPlainText = ""
+        isLastBlock = blockIndex == ctBlockLength - 2
+        winnerByte = ""
 
         for index in range(endPosition, startPosition, -2):
             step = (endPosition - index) / 2 + 1
             stepHex = chr(step).encode('hex')
-            pad = stepHex * step
             originalCipherByte = cipherText[index - 2:index]
 
             cipherAdjustment = getCipherAdjustment(stepHex, intermediateBlock)
 
             for trialByte in [chr(x).encode('hex') for x in range(0, 256)]:
 
-
+                winnerByte = None
                 firstPart = cipherText[0:index - 2]
                 lastPart = cipherText[endPosition:endPosition + 32]
                 queryString = firstPart + trialByte + cipherAdjustment + lastPart
-                print "{0} - {1} - {2} - {3}".format(firstPart, trialByte, cipherAdjustment, lastPart)
+                #print "{0} - {1} - {2} - {3}".format(firstPart, trialByte, cipherAdjustment, lastPart)
 
                 if len(queryString) % 32 != 0:
                     raise Exception("Submitting a query which is not a multiple of 32 HexEncoded")
@@ -51,20 +49,30 @@ def padAttack(cipherText):
                 queryResult = oracleQuery.query(queryString)
 
                 if queryResult:
-                    intermediateByte = util.hexxor(trialByte, stepHex)
-                    intermediateBlock = intermediateByte + intermediateBlock;
-                    plainTextByte = util.hexxor(originalCipherByte, intermediateByte)
-                    blockPlainText = plainTextByte + blockPlainText
-
-                    print "Match with guess={0}".format(trialByte)
-                    print "Intermediate byte={0}".format(intermediateByte)
-                    print "Plaint text byte={0}".format(plainTextByte)
+                    winnerByte = trialByte
                     break
 
-            if len(intermediateBlock) != len(pad):
-                raise Exception("No success finding a match for query")
+                # will not work for ct with exactly one byte pad
+                if isLastBlock and queryResult is None and step > 1:
+                    print "special 200 scenario"
+                    winnerByte = trialByte
+                    break
 
-        plaintText = blockPlainText + plaintText
+            if winnerByte is None:
+                raise Exception("No match found in last step")
+
+            intermediateByte = util.hexxor(winnerByte, stepHex)
+            intermediateBlock = intermediateByte + intermediateBlock;
+            plainTextByte = util.hexxor(originalCipherByte, intermediateByte)
+            blockPlainText = plainTextByte + blockPlainText
+
+            print "Original ct byte={0}".format(originalCipherByte)
+            print "Match with guess={0}".format(winnerByte)
+            print "Intermediate byte={0}".format(intermediateByte)
+            print "Plaint text byte={0}".format(plainTextByte)
+            print "Plain text block so far={0}", format(blockPlainText)
+
+        plaintText += blockPlainText
 
     return plaintText
 
