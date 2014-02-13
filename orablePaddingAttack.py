bab42ee53bc9d198a55d735141b155d49d6ddc56
bitoiu <vmrmonteiro@gmail.com>
2014-02-13 22:03:03 +0000

    Renaming files so it makes sense for visitors

--- orablePaddingAttack.py
+++ orablePaddingAttack.py
@@ -1,5 +1,5 @@
 import paddingQuery as pq
-import homework2 as util
+import cbcdecryption as util
 
 TARGET_CIPHERTEXT = "f20bdba6ff29eed7b046d1df9fb7000058b1ffb4210a580f748b4ac714c001bd4a61044426fb515dad3f21f18aa577c0bdf302936266926ff37dbf7035d5eeb4"
 BLOCK_SIZE_IN_BYTES = 16
